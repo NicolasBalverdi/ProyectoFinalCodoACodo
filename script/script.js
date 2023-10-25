259727a28b48a955801ff49a0f0e7e7e1d82c38f
Nico <nicolasbalverdi99@gmail.com>
2023-10-25 11:10:26 -0300

    Agregado de tabla

--- script/script.js
+++ script/script.js
@@ -1,3 +1,15 @@
+const empleados = [];
+
+function agregarNuevoEmpleado() {
+    // Limpia el formulario para ingresar datos de un nuevo empleado
+    document.getElementById('nombre').value = '';
+    document.getElementById('apellido').value = '';
+    document.getElementById('categoria').value = '';
+    document.getElementById('basico').value = '';
+    document.getElementById('fecha').value = '';
+    document.getElementById('hora-inicio').value = '';
+    document.getElementById('hora-fin').value = '';
+}
 function calcularYGuardar() {
     // Obtener los datos del formulario
     const nombre = document.getElementById('nombre').value;
@@ -20,7 +32,7 @@ function calcularYGuardar() {
     let nocturnas=0;
 
     const esFinDeSemana = fechaIngresada.getDay() === 5 || fechaIngresada.getDay() === 6;
-    const esDiaFeriado=esFeriado(fecha);
+    const esDiaFeriado=esFeriado(fechaIngresada);
     // Calcular las horas normales, extras al 50%, extras al 100% y nocturnas
     if (esFinDeSemana || esDiaFeriado) {
         extrasCien = horaFin-horaInicio;    
@@ -34,26 +46,98 @@ function calcularYGuardar() {
     // Calcular el sueldo total
     const sueldoTotal = valorHora * (horasNormales * 1 + extrasCincuenta * 1.5 + extrasCien * 2 + nocturnas * (17 / 15));
 
-    // Mostrar el "Valor Hora" en el campo correspondiente
-    document.getElementById('valor-hora').value = valorHora.toFixed(2);
+    empleados.push({
+        nombre: nombre,
+        apellido: apellido,
+        categoria: categoria,
+        fecha: fecha,
+        horasTrabajadas: `${horaInicio} - ${horaFin}`,
+        horasNormales: horasNormales,
+        extrasCincuenta: extrasCincuenta,
+        extrasCien: extrasCien,
+        horasNocturnas: nocturnas,
+        sueldoPagar: sueldoTotal.toFixed(2)
+    });
 
-    // Mostrar el resultado en la página
-    const resultado = `Nombre: ${nombre} ${apellido}, Categoría: ${categoria},Horas Normales:${horasNormales}, Extras al 50%:${extrasCincuenta}, 
-    Extras al 100%:${extrasCien}, Nocturnas:${nocturnas} Sueldo Total: ${sueldoTotal.toFixed(2)}, Fecha: ${fecha}`;
-    document.getElementById('resultado').innerText = resultado;
+    mostrarResultados();
 
-    // Opcional: Guardar los datos en un archivo y proporcionar un enlace para descargarlo
-    const blob = new Blob([resultado], { type: 'text/plain' });
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = 'datos.txt';
-    a.textContent = 'Descargar Datos';
-    document.getElementById('resultado').appendChild(a);
+
+    // // Mostrar el "Valor Hora" en el campo correspondiente
+    // document.getElementById('valor-hora').value = valorHora.toFixed(2);
+
+    // // Mostrar el resultado en la página
+    // const resultado = `Nombre: ${nombre} ${apellido}, Categoría: ${categoria},Horas Normales:${horasNormales}, Extras al 50%:${extrasCincuenta}, 
+    // Extras al 100%:${extrasCien}, Nocturnas:${nocturnas} Sueldo Total: ${sueldoTotal.toFixed(2)}, Fecha: ${fecha}`;
+    // document.getElementById('resultado').innerText = resultado;
+
+    // // Opcional: Guardar los datos en un archivo y proporcionar un enlace para descargarlo
+    // const blob = new Blob([resultado], { type: 'text/plain' });
+    // const url = URL.createObjectURL(blob);
+    // const a = document.createElement('a');
+    // a.href = url;
+    // a.download = 'datos.txt';
+    // a.textContent = 'Descargar Datos';
+    // document.getElementById('resultado').appendChild(a);
+}
+
+function mostrarResultados() {
+    const tabla = document.getElementById('tabla-resultados');
+    const tbody = tabla.querySelector('tbody');
+    // Limpiar la tabla antes de mostrar resultados para evitar duplicaciones
+    tbody.innerHTML = '';
+
+    for (const empleado of empleados) {
+        const fila = document.createElement('tr');
+        // Crear las celdas y asignar datos del empleado a cada celda
+        // Ejemplo de cómo asignar datos:
+        const celdaNombre = document.createElement('td');
+        celdaNombre.textContent = empleado.nombre;
+        fila.appendChild(celdaNombre);
+        
+        const celdaApellido = document.createElement('td');
+        celdaApellido.textContent = empleado.apellido;
+        fila.appendChild(celdaApellido);
+
+        const celdaCategoria = document.createElement('td');
+        celdaCategoria.textContent = empleado.categoria;
+        fila.appendChild(celdaCategoria);
+
+        const celdaFecha = document.createElement('td');
+        celdaFecha.textContent = empleado.fecha;
+        fila.appendChild(celdaFecha);
+
+        const celdaHorasTrabajadas = document.createElement('td');
+        celdaHorasTrabajadas.textContent = empleado.horasTrabajadas;
+        fila.appendChild(celdaHorasTrabajadas);
+
+        const celdaHorasNormales = document.createElement('td');
+        celdaHorasNormales.textContent = empleado.horasNormales;
+        fila.appendChild(celdaHorasNormales);
+
+        const celdaExtrasCincuenta = document.createElement('td');
+        celdaExtrasCincuenta.textContent = empleado.extrasCincuenta;
+        fila.appendChild(celdaExtrasCincuenta);
+
+        const celdaExtrasCien = document.createElement('td');
+        celdaExtrasCien.textContent = empleado.extrasCien;
+        fila.appendChild(celdaExtrasCien);
+
+        const celdaHorasNocturnas = document.createElement('td');
+        celdaHorasNocturnas.textContent = empleado.horasNocturnas;
+        fila.appendChild(celdaHorasNocturnas);
+
+        const celdaSueldoPagar = document.createElement('td');
+        celdaSueldoPagar.textContent = empleado.sueldoPagar;
+        fila.appendChild(celdaSueldoPagar);
+        // Agregar más celdas para otros campos, similar a celdaNombre
+
+        tbody.appendChild(fila);
+    }
 }
 
 function esFeriado(fecha) {
     const feriados = [
+        //Los (año, mes, dia) los meses estan al -1
         new Date(2023, 0, 1),  // Año Nuevo
         new Date(2023, 2, 24), // Carnaval
         new Date(2023, 2, 25), // Carnaval
@@ -64,7 +148,7 @@ function esFeriado(fecha) {
         new Date(2023, 6, 9),  // Día de la Independencia
         new Date(2023, 7, 21), // Día del Paso a la Inmortalidad del Gral. José de San Martín
         new Date(2023, 9, 16), // Día del Respeto a la Diversidad Cultural
-        new Date(2023, 12, 25),// Navidad
+        new Date(2023, 11, 25),// Navidad
     ];
 
     for (const feriado of feriados) {
@@ -107,3 +191,53 @@ function calcularValorHora() {
     document.getElementById('valor-hora').value = valorHora.toFixed(2);
 }
 
+// Crear un elemento <script>
+const scriptElement = document.createElement('script');
+
+// Establecer el atributo src con la URL de la biblioteca "xlsx"
+scriptElement.src = 'https://cdn.jsdelivr.net/npm/xlsx@0.18.0/dist/xlsx.full.min.js';
+
+// Definir una función que se ejecutará una vez que se cargue la biblioteca
+scriptElement.onload = function() {
+    // La biblioteca "xlsx" estará disponible aquí
+    console.log('La biblioteca "xlsx" se ha cargado correctamente.');
+    // Puedes usar xlsx aquí para exportar a Excel u otras tareas relacionadas con la biblioteca.
+};
+
+// Agregar el elemento <script> al documento (esto iniciará la descarga de la biblioteca)
+document.body.appendChild(scriptElement);
+
+document.getElementById('exportExcelButton').addEventListener('click', function () {
+    const empleados = [
+        // Aquí debes proporcionar tus datos de empleados en la estructura correspondiente
+    ];
+
+    // Crear una hoja de cálculo en formato XLSX
+    const ws = XLSX.utils.json_to_sheet(empleados);
+
+    // Crear un libro de trabajo y adjuntar la hoja de cálculo
+    const wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, "Empleados");
+
+    // Generar un Blob (objeto binario grande) a partir del libro de trabajo
+    const blob = XLSX.write(wb, { bookType: "xlsx", type: "blob" });
+
+    // Crear un objeto URL para el Blob
+    const url = URL.createObjectURL(blob);
+
+    // Crear un enlace para descargar el archivo Excel
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "empleados.xlsx"; // Nombre del archivo
+    a.style.display = "none";
+
+    // Agregar el enlace al documento y hacer clic automáticamente
+    document.body.appendChild(a);
+    a.click();
+
+    // Limpiar el objeto URL y eliminar el enlace después de la descarga
+    window.URL.revokeObjectURL(url);
+    document.body.removeChild(a);
+
+    
+});
